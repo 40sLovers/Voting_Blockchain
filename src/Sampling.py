b422a8b2744d0c11ce2f8d7dc65b8ef36cbed397
gandolh <gusatualex@yahoo.com>
2021-12-07 22:21:41 +0200

    testing stuffs

--- src/Sampling.py
+++ src/Sampling.py
@@ -1,5 +1,6 @@
 import sys, os
-
+#maybe it works only with python 3.9?
+sys.path.append(os.path.abspath(os.path.join('..','libraries','elliptic-py','src')))
 from ellipticpy import SigningKey, SECP256k1
 sk = SigningKey.generate(curve = SECP256k1)
 sk_string = sk.to_string()
