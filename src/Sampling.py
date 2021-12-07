cb65a113666d6cacc62a61833e0d40e14ad2a42a
gandolh <gusatualex@yahoo.com>
2021-12-07 22:15:47 +0200

    stergere .idea si adaugarea ei in gitignore

--- src/Sampling.py
+++ src/Sampling.py
@@ -1,5 +1,6 @@
 import sys, os
-sys.path.append(os.path.abspath(os.path.join('..','libraries','elliptic-py','src')))
+
 from ellipticpy import SigningKey, SECP256k1
 sk = SigningKey.generate(curve = SECP256k1)
 sk_string = sk.to_string()
+print(sk_string)
