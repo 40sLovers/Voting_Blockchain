94fcd66d47a5a185136b8d0e1b4263b1d8cadbef
Octa2324 <95917752+Octa2324@users.noreply.github.com>
2021-12-10 17:18:10 +0200

    creare functie logare

---
+++ frontend/js/jsfrontend/functie logare.js
@@ -0,0 +1,13 @@
+function myObject(){
+    
+    const d={
+        email:document.getElementById("ad");
+        nrmatricol:document.getElementById("nrm");
+        cheie:document.getElementById("pass");
+    }
+        
+    
+    return d;
+
+        
+}
