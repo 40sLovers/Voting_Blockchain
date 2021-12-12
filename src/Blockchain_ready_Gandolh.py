17d599fee0208251ab6ed894f62ab396c9eea648
gandolh <gusatualex@yahoo.com>
2021-12-12 11:31:00 +0200

    Organize the code a little

--- src/Blockchain_ready_Gandolh.py
+++ src/Blockchain_ready_Gandolh.py
@@ -1,9 +1,8 @@
-from Block import Block
-from Transaction import Transaction
 from datetime import datetime
-
+from ecpy.ecdsa import ECDSA
+import GoodToUseScripts
+import uuid
 class Blockchain:
-
     def __init__(self):
         self.chain = [self.createGenesisBlock(),]
         self.pendingTransactions = []
@@ -80,14 +79,60 @@ class Blockchain:
         return True
 
 
-if __name__== "__main__":
-    #cod pentru blockchain.py
-    bloc = Blockchain()
-    print(bloc.chain)
+class Block:
+    def __init__(self, timestamp, transactions, previousHash,guid = str(uuid.uuid4())):
+        # timestmap = cand a fost creat
+        self.timestamp = timestamp
+        self.transactions = transactions
+        self.previousHash = previousHash
+        self.guid= guid
+        self.hash = self.calculateHash()
+        self.nonce = 0
 
-    print(bloc.getLatestBlock())
-    bloc.chain.append(Block("26/07/2001 00:00:00", [], None))
-    print(bloc.getLatestBlock())
-    bloc.chain.append(Block("26/07/2000 00:00:00", [], None))
-    print(bloc.getLatestBlock())
+    def calculateHash(self):
+        return GoodToUseScripts.updatehash(self.timestamp, self.transactions, self.previousHash)
 
+    def mineBlock(self, difficulty=2):
+        while self.hash[0, difficulty] != [0] * difficulty:
+            self.nonce += 1
+            self.hash = self.calculateHash()
+
+    def hasValidTransactions(self):
+        for tx in self.transactions:
+            if not tx.isValid():
+                return False
+        return True
+
+    def __repr__(self):
+        return (self.timestamp + " " + str(self.transactions) + " " + str(self.previousHash))
+
+
+class Transaction:
+    def __init__(self, fromAdress, toAdress, amount):
+        self.fromAdress = fromAdress
+        self.toAdress = toAdress
+        self.amount = amount
+        self.timestamp = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+
+    def calculateHash(self):
+        concatenatedString= str(self.fromAdress)+str(self.toAdress)+str(self.amount)+str(self.timestamp)
+        return str.encode(GoodToUseScripts.updatehash(concatenatedString))
+
+    def SignTransaction(self,signingKey):
+        #signingKey e o cheie privata.
+        if signingKey.get_public_key().x != self.fromAdress.x or \
+        signingKey.get_public_key().y != self.fromAdress.y:
+            raise Exception("Nu poti semna tranzactii pentru alte portofele")
+        hashTx= self.calculateHash()
+        signer = ECDSA()
+        sig = signer.sign(hashTx,signingKey)
+        self.signature = sig
+
+    def isValid(self):
+        # If the transaction doesn't have a from address we assume it's a
+        # mining reward and that it's valid. 
+        if self.fromAddress == None: return True
+        if not self.signature:
+            raise Exception("Nici-o semnatura in aceasta tranzactie")
+        signer = ECDSA()
+        return signer.verify(self.calculateHash(), self.sig, self.fromAdress)
--- src/GoodToUseScripts.py
+++ src/GoodToUseScripts.py
@@ -1,5 +1,8 @@
 from hashlib import sha256
 from datetime import datetime
+from ecpy.keys       import ECPublicKey, ECPrivateKey
+from ecpy.curves     import Curve,Point
+
 def updatehash(*args):
     hashing = ""
     h = sha256()
@@ -7,3 +10,11 @@ def updatehash(*args):
         hashing += str(arg)
     h.update(hashing.encode('utf-8'))
     return h.hexdigest()
+
+
+def keyFromHash(hash):
+    cv= Curve.get_curve('secp256k1')
+    hashInteger=hash.encode('utf-8').hex()
+    userPvKey= ECPrivateKey(hashInteger,
+                      cv)
+    return userPvKey
