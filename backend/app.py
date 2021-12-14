f9103533036bbf205617fe385b42225900808927
haiosmichelle <94860504+haiosmichelle@users.noreply.github.com>
2021-12-14 17:04:02 +0200

    email

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,53 @@
 from flask import Flask,request,render_template
+from flask_mail import Mail,Message
 
 WhiteList = []
 
+
 app = Flask(__name__)
+mail=Mail(app)
+app.config["MAIL_SERVER"]="smpt.gmail.com"
+app.config["MAIL_PORT"]=465
+app.config["MAIL_USERNAME"]="p1projectprogram@gmail.com"
+app.config["MAIL_PASSWORD"]="Project123"
+app.config['MAIL_USE_SSL'] = True
+
+
+
+mail=Mail(app)
 @app.route('/')
 def login3():
-    return render_template('logareC.html')
+    return render_template('inregistrare.html')
 
 
 @app.route('/', methods=['POST'])
 def login2():
     #registerData = request.get_json()
+    validareemail=request.json_get("validareemail",None)
     criptare = request.json_get("criptare", None)
     email = request.json_get("adresa", None)
     numarMatricol = request.json_get("numarMatricol", None)
     cuvantCheie = request.json_get("cuvantCheie", None)
     WhiteList.append( criptare)
     return criptare
-
+@app.route("/inregistrare", methods=['POST','GET'])
+def log(result):
+   validareemail=request.json_get("validareemail",None)
+   if request.method == 'POST' and validareemail==True:
+     email = request.json_get("adresa", None)
+     msg=Message(request.form.get("hi"),sender="p1project@gmail.com",recipients=email)
+     msg.body="buna ce mai faci Onisim<3?"
+     mail.send(msg)
+     
+@app.route("/inregistrare")
+def inregistrare()
+{
+    if request.method == 'POST':
+        result['email']=request.form['email'].replace(" "," ").lower()
+        result['message']=request.form['message']
+        log(result)
+        return render_template("confirmare.html")
+}  
 @app.route('/<name>')
 def login(name):
     if(name=="inregistrare"):
