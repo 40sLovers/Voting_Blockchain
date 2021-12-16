f1345f3854d63d8429504e0236cc6d7f61ce1eff
OnisimBalan <onisim.balan99@e-uvt.ro>
2021-12-16 18:42:22 +0200

    sters frontend e inutil

--- frontend/js/functie logare.js
+++
@@ -1,8 +0,0 @@
-function myObject(){
-    const d = new Object();
-    d.email = document.getElementById("ad")
-    d.nrmatricol = document.getElementById("nrm")
-    d.parola = document.getElementById("pass")
-        
-    return d;  
-}
